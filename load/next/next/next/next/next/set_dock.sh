cb3873cb174895062918f89d905f0e9cc6fbff3d
Nik <nkotikov@gmail.com>
2024-07-17 18:46:29 +0300

    change dock & workspaces behavior

--- load/next/next/next/next/next/set_dock.sh
+++ load/next/next/next/next/next/set_dock.sh
@@ -7,3 +7,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.5
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 48
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'cycle-windows'
+# Dock on primary display only
+gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor false
+
