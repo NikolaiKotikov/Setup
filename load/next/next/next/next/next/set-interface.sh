887fa1f1e5e79ae1faacf3bd44b625e94c09bec8
nik <nikolai.kotikov@gmail.com>
2024-07-22 00:00:55 +0300

    optional apps & ui fix
    - change vbox installation method
    - add zed to optional apps
    - show windows tiles by default

--- load/next/next/next/next/next/set-interface.sh
+++ load/next/next/next/next/next/set-interface.sh
@@ -1,5 +1,4 @@
 gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
-gsettings set org.gnome.shell.extensions.pop-shell show-title false
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.mutter workspaces-only-on-primary true
--- load/optional/install_vbox.sh
+++ load/optional/install_vbox.sh
@@ -1,2 +1,9 @@
-sudo apt install -y virtualbox virtualbox-ext-pack
-sudo usermod -aG vboxusers ${USER}
+cd "$HOME"/Downloads
+
+wget https://download.virtualbox.org/virtualbox/7.0.20/virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb -O virtualbox.deb
+
+sudo apt install -y ./virtualbox.deb
+
+rm virtualbox.deb
+
+vboxmanage --version
