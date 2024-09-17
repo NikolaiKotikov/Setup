dbc412d1d022951933f3cd2967e5055cd9e11d02
nkotikov <nkotikov@moysklad.ru>
2024-09-17 17:31:56 +0300

    do not tile by default

--- load/next/next/next/next/next/set-interface.sh
+++ load/next/next/next/next/next/set-interface.sh
@@ -1,4 +1,3 @@
-gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.mutter workspaces-only-on-primary true
