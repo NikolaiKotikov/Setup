35eaf6a52deec0da8bc623b3c1dd876f224a7003
nkotikov <nkotikov@moysklad.ru>
2024-09-18 15:07:53 +0300

    same input source for all windows

--- load/next/next/next/next/next/set-interface.sh
+++ load/next/next/next/next/next/set-interface.sh
@@ -1,5 +1,4 @@
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.mutter workspaces-only-on-primary true
-gsettings set org.gnome.desktop.input-sources per-window true
 gsettings set org.gnome.mutter center-new-windows true
